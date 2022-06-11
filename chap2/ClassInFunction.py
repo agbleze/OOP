5ada96ee96711eb33b369d74e724c546cb1233e2
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-11 07:57:42 +0200

    Create ClassInFunction.py

---
+++ chap2/ClassInFunction.py
@@ -0,0 +1,33 @@
+#%%
+from typing import Optional
+
+class Formatter:
+    def format(self, string: str) -> str:
+        pass
+    
+def format_string(string: str, 
+                  formatter: Optional[Formatter] = None
+                  )->str:
+    """
+    Format a string using the formatter object, which is expected
+    to have a format() method that accepts a string.
+    """
+    
+    class DefaultFormatter(Formatter):
+        """
+        Format a string with a title case
+        """
+        def format(self, string: str) -> str:
+            return str(string).title()
+        
+    if not formatter:
+        formatter = DefaultFormatter()
+    return formatter.format(string)
+ 
+ 
+#%%
+hello_string = 'hello world, how are you doing?'
+print(f" input: {hello_string}")   
+# %%
+print(f"output: {format_string(hello_string)}")
+# %%
