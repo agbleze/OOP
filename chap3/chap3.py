650241334024321755cf2e4231ced6b79724293f
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-12 15:12:41 +0200

    Update chap3.py

--- chap3/chap3.py
+++ chap3/chap3.py
@@ -1,3 +1,7 @@
+#%% 
+from typing import List
+
+#%%
 class Contact:
     all_contacts: List["Contact"] = []
     
@@ -12,3 +16,18 @@ class Contact:
                 f"{self.name!r}, {self.email!r}"
                 f")"
             )
+        
+#%%
+c_1 = Contact('Dusty', 'dusty@example.com')
+c_2 = Contact('Steve', 'steeve@example.com')
+print(Contact.all_contacts)
+
+#%%
+class Supplier(Contact):
+    def order(self, order: "Order") -> None:
+        print("If this were a real systems we would send "
+              f"'{order}' order to '{self.name}'"
+              
+            )
+
+# %%
