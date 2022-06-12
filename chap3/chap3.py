ccde537e704a9dd48f84384152032b892bc6eee3
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-13 01:27:18 +0200

    Update chap3.py

--- chap3/chap3.py
+++ chap3/chap3.py
@@ -166,6 +166,30 @@ class AudioFile:
     def __init__(self, filepath: Path) -> None:
         if not filepath.suffix == self.ext:
             raise ValueError("Invalid file format")
+        self.filepath = filepath
+        
+class MP3File(AudioFile):
+    ext = ".mp3"
+    
+    def play(self) -> None:
+        print(f"playing {self.filepath} as mp3")
+        
+class WavFile(AudioFile):
+    ext = ".wav"
+    
+    def play(self) -> None:
+        print(f"playing {self.filepath} as wav")
+        
+class OggFile(AudioFile):
+    ext = ".ogg"
+    
+    def play(self) -> None:
+        print(f"playing {self.filepath} as ogg")
 
+#%%
+p1 = MP3File(Path("Heart of the sunrise.mp3"))
+p1.play()
 
-
+p2 = WavFile(Path("Roundabout.wav"))
+p2.play()
+# %%
