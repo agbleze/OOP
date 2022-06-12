7d63bae184e20437470999bd844f15bb777f7423
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-13 01:43:36 +0200

    Update chap3.py

--- chap3/chap3.py
+++ chap3/chap3.py
@@ -1,8 +1,7 @@
 #%% 
 from __future__ import annotations
-from typing import List, Any
-
-from pyparsing import Optional
+from typing import List, Any, Optional
+from math import hypot
 
 #%%
 class ContactList(list["Contact"]):
@@ -192,4 +191,24 @@ p1.play()
 
 p2 = WavFile(Path("Roundabout.wav"))
 p2.play()
+
+
+######################## case study  ##########################
 # %%
+class ED(Distance):
+    def distance(self, s1: "Sample", s2: "Sample") -> float:
+        return hypot(
+            s1.sepal_length - s2.sepal_length,
+            s1.sepal_width - s2.sepal_width,
+            s1.petal_length - s2.petal_length,
+            s1.petal_width - s2.petal_width,
+        )
+
+
+
+
+
+
+
+
+
