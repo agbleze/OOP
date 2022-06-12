1f2e426b96ce21ac7e984a64b856c4eb205784cc
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-12 19:08:44 +0200

    Update chap3.py

--- chap3/chap3.py
+++ chap3/chap3.py
@@ -1,9 +1,19 @@
 #%% 
+from __future__ import annotations
 from typing import List
 
+from pyparsing import Optional
+
 #%%
+class ContactList(list["Contact"]):
+    def search(self, name: str) -> list["Contact"]:
+        matching_contacts: list["Contact"] = []
+        for contact in self:
+            if name in contact.name:
+                matching_contacts.append(contact)
+            return matching_contacts
 class Contact:
-    all_contacts: List["Contact"] = []
+    all_contacts: ContactList()
     
     def __init__(self, name: str, email: str) -> None:
         self.name = name
@@ -18,10 +28,25 @@ class Contact:
             )
         
 #%%
+Contact.all_contacts = ContactList()
 c_1 = Contact('Dusty', 'dusty@example.com')
 c_2 = Contact('Steve', 'steeve@example.com')
-print(Contact.all_contacts)
+c3 = Contact("Jenna", "cutty@sark.io")
 
+[c.name for c in Contact.all_contacts.search('Dusty')]
+
+#print(Contact.all_contacts)
+
+
+#%%
+test_search = """
+>>> Contact.all_contacts = ContactList()
+>>> c1 = Contact("John A", "johna@example.net")
+>>> c2 = Contact("John B", "johnb@sloop.net")
+>>> c3 = Contact("Jenna C", "cutty@sark.io")
+>>> [c.name for c in Contact.all_contacts.search('John')]
+['John A', 'John B']
+"""
 #%%
 class Supplier(Contact):
     def order(self, order: "Order") -> None:
@@ -38,8 +63,34 @@ print(c.name, c.email, s.name, s.email)
 from pprint import pprint
 pprint(c.all_contacts)
 
+#%%
+class LongNameDict(dict[str, int]):
+    def longest_key(self) -> Optional[str]:
+        """In effect max(self, key=len), but less obscure"""
+        longest = None
+        for key in self:
+            if longest is None or len(key) > len(longest):
+                longest = key
+        return longest
 
+#%%
+articles_read = LongNameDict()
+articles_read['lucy'] = 42
+articles_read['c_c_philips'] = 6
+articles_read['steve'] = 7
+articles_read.longest_key()
 
+max(articles_read, key=len)
 
+# %%  
+# OVERRIDING nd super
+# multiple inheritance
 
+class Friend(Contact):
+    def __init__(self, name: str, email: str, phone: str) -> None:
+        super().__init__(name, email)
+        self.phone = phone
+# %%
+f = Friend("Dusty", "Dusty@private.com", "555-1212")
+Contact.all_contacts
 # %%
