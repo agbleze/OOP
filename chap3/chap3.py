3bcd101bfe2fba9c9a265a4e725ce2c44151d618
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-13 02:28:24 +0200

    Update chap3.py

--- chap3/chap3.py
+++ chap3/chap3.py
@@ -204,11 +204,50 @@ class ED(Distance):
             s1.petal_width - s2.petal_width,
         )
 
+class Distance:
+    """Definition of a distance computation"""
+    def distance(self, s1: Sample, s2: Sample) -> float:
+        pass
+    
+class MD(Distance):
+    def distance(self, s1: Sample, s2: Sample) -> float:
+        return sum(
+            [
+                abs(s1.sepal_length - s2.sepal_length),
+                abs(s1.sepal_width - s2.sepal_width),
+                abs(s1.petal_length - s2.petal_length),
+                abs(s1.petal_width - s2.petal_width),
+            ]
+        )
+
+class CD(Distance):
+    def distance(self, s1: Sample, s2: Sample) -> float:
+        return sum(
+            [
+                abs(s1.sepal_length - s2.sepal_length),
+                abs(s1.sepal_width - s2.sepal_width),
+                abs(s1.petal_length - s2.petal_length),
+                abs(s1.petal_width - s2.petal_width),
+            ]
+        )
 
 
-
-
-
-
+class SD(Distance):
+    def distance(self, s1: Sample, s2: Sample):
+        return sum(
+            [
+                abs(s1.sepal_length - s2.sepal_length),
+                abs(s1.sepal_width - s2.sepal_width),
+                abs(s1.petal_length - s2.petal_length),
+                abs(s1.petal_width - s2.petal_width),
+            ]
+        ) / sum(
+            [
+                s1.sepal_length + s2.sepal_length,
+                s1.sepal_width + s2.sepal_width,
+                s1.petal_length + s2.petal_length,
+                s1.petal_width + s2.petal_width,
+            ]
+        )
 
 
