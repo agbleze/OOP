66be498f20dc1d0c36e8423de7e0675cd952be43
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-14 14:38:40 +0200

    Update error_handling.py

--- chap4/error_handling.py
+++ chap4/error_handling.py
@@ -72,8 +72,20 @@ for choice in some_exceptions:
 # %% defining own exception
 from decimal import Decimal
 class InvalidWithdrawal(ValueError):
-    pass
+    def __init__(self, balance: Decimal, amount: Decimal)->None:
+        super().__init__(f"account doesnt have ${amount}")
+        self.amount = amount
+        self.balance = balance
+        
+    def overage(self) -> Decimal:
+        return self.amount - self.balance
 
-raise InvalidWithdrawal('Dont have $50 in account')
-
+# %%
+try:
+    balance = Decimal('25.00')
+    raise InvalidWithdrawal(balance, Decimal('50.00'))
+except InvalidWithdrawal as ex:
+    print("I'm sorry, but your withdrawal is "
+          "more than your balance by "
+          f"${ex.overage()}")
 # %%
