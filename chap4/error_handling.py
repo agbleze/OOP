5d86c7445150d0d48d62911ddc80bf8471e75860
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-14 13:09:50 +0200

    Update error_handling.py

--- chap4/error_handling.py
+++ chap4/error_handling.py
@@ -45,6 +45,35 @@ try:
 except ValueError as e:
     print(f"The exception arguments were {e.args}")
 
+#%%
+some_exceptions = [ValueError, TypeError, IndexError, None]
 
+for choice in some_exceptions:
+    try:
+        print(f"\nRaising {choice}")
+        if choice:
+            raise choice("An error")
+        else:
+            print("no exception raised")
+    except ValueError:
+        print("Caught a ValueError")
+    except TypeError:
+        print("Caught a TypeError")
+    except Exception as e:
+        print(f"Caught some other error: {e.__class__.__name__}")
+    else:
+        print("This code called if there is no exception")
+    finally:
+        print("This cleanup code is always called")
+
+
+
+
+# %% defining own exception
+from decimal import Decimal
+class InvalidWithdrawal(ValueError):
+    pass
+
+raise InvalidWithdrawal('Dont have $50 in account')
 
 # %%
