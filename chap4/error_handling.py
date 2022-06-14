2abbb345ba9b268194cd8596c55d72cdcf00b8ed
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-14 15:18:25 +0200

    Update error_handling.py

--- chap4/error_handling.py
+++ chap4/error_handling.py
@@ -1,6 +1,6 @@
 #%%
 
-from typing import List
+from typing import List, Optional
 class EvenOnly(List[int]):
     def append(self, value: int) -> None:
         if not isinstance(value, int):
@@ -110,11 +110,89 @@ def from_dict(cls, row: dict[str, str]) -> "KnownSample":
     except ValueError as ex:
         raise InvalidSampleError(f"invalid {row!r}")
     
-    
+#%%
+class Sample:
+    def __init__(self, sepal_length: float, 
+                 sepal_width: float,
+                 petal_length: float, 
+                 petal_width: float,
+                 species: Optional[str]= None
+                 )-> None:
+        self.sepal_length = sepal_length,
+        self.sepal_width = sepal_width,
+        self.petal_length = petal_length,
+        self.petal_width = petal_width,
+        self.species = species,
+        self.classification: Optional[str] = None
+        
+        
+    def __repr__(self)->str:
+        if self.species is None:
+            known_unknown = "UnknownSample"
+        else:
+            known_unknown = "KnownSample"
+            
+        if self.classification is None:
+            classification = ""
+        else:
+            classification = f", {self.classification}"
+        
+        return (
+            f"{known_unknown}("
+            f"sepal_length={self.sepal_length}, "
+            f"sepal_width={self.sepal_width}, "
+            f"petal_length={self.petal_length} "
+            f"petal_width={self.petal_width}, "
+            f"species={self.species!r}"
+            f"{classification}"
+            f")"
+        )
+        
+    def classify(self, classification: str) -> None:
+        self.classification = classification
+        
+    def matches(self) -> bool:
+        return self.species == self.classification
+        
+class KnownSample(Sample):
+    def __init__(self,
+                 species: str,
+                 sepal_length: float, 
+                 sepal_width: float,
+                 petal_length: float, 
+                 petal_width: float,
+                 ) -> None:
+        super().__init__(
+            sepal_length = sepal_length,
+            sepal_width = sepal_width,
+            petal_length = petal_length,
+            petal_width = petal_width,
+        )
+        self.species = species
+        
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}("
+            f"sepal_length={self.sepal_length}, "
+            f"sepal_width={self.sepal_width}, "
+            f"petal_length={self.petal_length}, "
+            f"petal_width={self.petal_width}, "
+            f"species={self.species!r}"
+            f")"
+        )
 class TrainingKnownSample(KnownSample):
     @classmethod
     def from_dict(cls, row: dict[str, str]) ->  "TrainingKnownSample":
         return cast(TrainingKnownSample, super().from_dict(row))   
     
-    
-    
+# %%
+valid = {"sepal_length": "5.1", "sepal_width": "3.5", 
+         "petal_length": "1.4", "petal_width": "0.2",
+         "species": "Iris-setosa"}
+
+
+
+
+
+
+# %%
