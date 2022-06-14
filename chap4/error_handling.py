50fef7005ed866d6ef6538288cefa52a601951b3
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-14 12:21:53 +0200

    Update error_handling.py

--- chap4/error_handling.py
+++ chap4/error_handling.py
@@ -15,4 +15,36 @@ e.append("a string")
 e.append(3)
 # %%
 e.append(2)
+
+# %%
+from typing import Union
+
+#%%
+def funny_division(divisor: float) -> Union[str, float]:
+    try:
+        if divisor == 13:
+            raise ValueError("13 is an unlucky")
+        return 100 / divisor
+    except (ZeroDivisionError, TypeError):
+        return "Enter a number other than zero"
+
+#%%
+print(funny_division(0))
+print(funny_division(50.0))
+
+funny_division("hello world")
+
+# %%
+for val in (0, 'hello', 50.0, 13):
+    print(f"Testing {val!r}:", end=" ")
+    print(funny_division(val))
+
+#%%
+try:
+    raise ValueError("This is an argument")
+except ValueError as e:
+    print(f"The exception arguments were {e.args}")
+
+
+
 # %%
