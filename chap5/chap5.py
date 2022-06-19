d8f7bcdf7a136d7aaea6fad5b6e2535c1e94c0f4
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-19 22:23:29 +0200

    Update chap5.py

--- chap5/chap5.py
+++ chap5/chap5.py
@@ -7,7 +7,11 @@ Point = Tuple[float, float]
 def distance(p_1: Point, p_2: Point) -> float:
     return hypot(p_1[0] - p_2[0], p_1[1] - p_2[1])
 
+Polygon = List[Point]
 
+def perimeter(polygon: Polygon) -> float:
+    pairs = zip(polygon, polygon[1:] + polygon[:1])
+    return sum(distance(p1, p2) for p1, p2 in pairs)
 
 
 
