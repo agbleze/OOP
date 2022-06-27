ef89a25098066083a01ea3bdf74e29fe15a97c05
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-27 15:15:11 +0200

    Update chap5.py

--- chap5/chap5.py
+++ chap5/chap5.py
@@ -192,11 +192,51 @@ class NorwegianBlue_P:
 from urllib.request import urlopen
 from typing import Optional, cast
 
-    
-    
-    
-    
+class WebPage:
+    def __init__(self, url: str) -> None:
+        self.url = url
+        self._content: Optional[bytes] = None
+        
+    @property
+    def content(self) -> bytes:
+        if self._content is None:
+            print("Retrieving New Page...")
+            with urlopen(self.url) as response:
+                self._content = response.read()
+        return self._content
 
+#%%
+import time
+
+#%%
+webpage = WebPage("http://ccphillips.net/") 
+
+now = time.perf_counter()
+content1 = webpage.content
+first_fetch = time.perf_counter() - now
+
+#%%
+now = time.perf_counter()
+content2 = webpage.content
+second_fetch = time.perf_counter() - now
+   
+assert content2 == content1, "Problem: Pages were different"
+print(f"Initial Request {first_fetch: .5f}")
+print(f"Subsequent Request {second_fetch: .5f}")
+    
+#%%
+from typing import List
+
+class AverageList(List[int]):
+    @property
+    def average(self) -> float:
+        return sum(self) / len(self)
+
+#%%    
+a = AverageList([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
+a.average   
+
+#%% Manage objects
 
 
 
