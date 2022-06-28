d1975b00742ce1f1a8952504eec73aa0f5b82c94
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-29 00:58:22 +0200

    Create chap6.py

---
+++ chap6/chap6.py
@@ -0,0 +1,20 @@
+#%%
+import abc
+
+class MediaLoader(abc.ABC):
+    @abc.abstractmethod
+    def play(self) -> None:
+        ...
+        
+    @property
+    @abc.abstractmethod
+    def ext(self) -> str:
+        ...
+
+# %%
+class Wav(MediaLoader):
+    pass
+# %%
+x = Wav()
+
+# %%
