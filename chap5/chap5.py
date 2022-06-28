d9f2539c9710b27175005052faa02b37afd33b38
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-28 04:53:29 +0200

    Update chap5.py

--- chap5/chap5.py
+++ chap5/chap5.py
@@ -242,26 +242,66 @@ import fnmatch
 from pathlib import Path
 import re
 import zipfile
+from abc import ABC, abstractmethod
 
-class ZipReplace:
+class ZipProcessor(ABC):
     def __init__(
         self,
-        archive: Path,
-        pattern: str,
-        find: str,
-        replace: str
+        archive: Path
     ) -> None:
         self.archive_path = archive,
-        self.pattern = pattern,
-        self.find = find,
-        self.replace = replace
+        self._pattern: str
         
-    def find_and_replace(self) -> None:
+    def process_files(self, pattern: str) -> None:
+        self._pattern = pattern
         input_path, output_path = self.make_backup()
         
         with zipfile.ZipFile(output_path, "w") as output:
             with zipfile.Zipfile(input_path) as input:
                 self.copy_and_transform(input, output)
+                
+    def make_backup(self) -> tuple[Path, Path]:
+        input_path = self.archive_path.with_suffix(
+            f"{self.archive_path.suffix}.old"
+        )
+        output_path = self.archive_path
+        self.archive_path.rename(input_path)
+        return input_path, output_path
+    
+    def copy_and_transform(
+        self, input: zipfile.ZipFile, output: zipfile.ZipFile
+    ) -> None:
+        for item in input.infolist():
+            extracted = Path(input.extract(item))
+            if self.matches(item):
+                print(f"Transform {item}")
+                self.transform(extracted)
+            else:
+                print(f"Ignore {item}")
+            output.write(extracted, item.filename)
+            self.remove_under_cwd(extracted)
+            
+            
+            if (not item.is_dir() and fnmatch(item.filename, self.pattern)):
+                print(f"Transform {item}")
+                input_text = extracted.read_text()
+                output_text = re.sub(self.find, self.replace, self.pattern)
+                extracted.write_text(output_text)
+            else:
+                print(f"Ignore {item}")
+                output.write(extracted, item.filename)
+                extracted.unlink()
+                for parent in extracted.parents:
+                    if parent == Path.cwd():
+                        break
+                    parent.rmdir()
+                
+                
+if __name__ == "__main__":
+    sample_zip = Path("sample.zip")
+    zr = ZipReplace(sample_zip, "*.md", "xyzzy", "plover's egg")
+    zr.find_and_replace()
+                
 
 
 
