1e7c3036639dd4f3dcde0e0edbdd0174e6df376c
Agbleze <lagbleze@st.ug.edu.gh>
2022-07-01 03:33:54 +0200

    Update chap6.py

--- chap6/chap6.py
+++ chap6/chap6.py
@@ -184,8 +184,61 @@ class DieM(metaclass = abc.ABCMeta):
     def roll(self) -> None:
         ...
 
+#%% Operator overloading
+# deals with expanding operators to work with other data types
+from pathlib import Path
+home = Path.home()
+home / "minconda3" / "envs"
+# the / operator is used to connect a Path object to
+# a string object to create new Path object
 
-
+#%%
+class DDice:
+    def __init__(self, *die_class: Type[Die]) -> None:
+        self.dice = [dc() for dc in die_class]
+        self.adjust: int = 0
+    
+    def plus(self, adjust: int = 0) -> "DDice":
+        self.adjust = adjust
+        return self
+    
+    def roll(self) -> None:
+        for d in self.dice:
+            d.roll()
+            
+    @property
+    def total(self) -> int:
+        return sum(d.face for d in self.dice) + self.adjust
+    
+    def __add__(self, die_class: Any) -> "DDice":
+        if isinstance(die_class, type) and issubclass(die_class, Die):
+            new_classes = [type(d) for d in self.dice] + [die_class]
+            new = DDice(*new_classes).plus(self.adjust)
+            return new
+        elif isinstance(die_class, int):
+            new_classes = [type(d) for d in self.dice]
+            new = DDice(*new_classes).plus(die_class)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
 
 
 
