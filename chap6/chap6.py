c864e9a9ba34d4752abe76440d7135cebc825656
Agbleze <lagbleze@st.ug.edu.gh>
2022-07-02 23:46:14 +0200

    Update chap6.py

--- chap6/chap6.py
+++ chap6/chap6.py
@@ -370,6 +370,18 @@ class SamplePartition(List[SampleDict], abc.ABC):
                  training_subset: float = 0.80
                  ) -> None:
         ...
+        
+    def __init__(
+        self,
+        iterable: Optional[Iterable[SampleDict]] = None,
+        *,
+        training_subset: float = 0.80
+    ) -> None:
+        self.training_subset = training_subset
+        if iterable:
+            super().__init__(iterable)
+        else:
+            super().__init__()
  
  
  
