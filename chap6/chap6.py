5fb345c134d711fa09a385c7f1bfd58690c4a341
Agbleze <lagbleze@st.ug.edu.gh>
2022-07-03 02:52:10 +0200

    Update chap6.py

--- chap6/chap6.py
+++ chap6/chap6.py
@@ -454,6 +454,33 @@ class DealingPartition(abc.ABC):
     @abc.abstractmethod
     def testing(self) -> List[TestingKnownSample]:
         ...
+        
+class CountingDealingPartition(DealingPartition):
+    def __init__(
+        self,
+        items: Optional[Iterable[SampleDict]],
+        *,
+        training_subset: Tuple[int, int] = (8, 10),
+        
+    ) -> None:
+        self.training_subset = training_subset
+        self.counter = 0
+        self._training: List[TrainingKnownSample] = []
+        self._testing: List[TestingKnownSample] = []
+        if items:
+            self.extend(items)
+    
+    def extend(self, items: Iterable[SampleDict]) -> None:
+        for item in items:
+            self.append(item)
+            
+    def append(self, item: SampleDict) -> None:
+        n, d = self.training_subset
+        if self.counter % d < n:
+            self._training.append(TrainingKnownSample(**item))
+        else:
+            self._testing.append(TestingKnownSample(**item))
+        self.counter += 1
  
  
     
