459a4d07e0962a57c02006536fb34f27393869eb
Agbleze <lagbleze@st.ug.edu.gh>
2022-09-12 22:58:00 +0200

    Update chap7.py

--- chap7/chap7.py
+++ chap7/chap7.py
@@ -5,3 +5,48 @@ o = object()
 # %%
 o.x = 5
 # %%
+import random
+# %%
+random.randint(5, 500)
+# %%
+from typing import List
+import random as rd
+def countsums(wuerfel: int, wuerfe: int, seiten: int) -> List[int]:
+    A = []
+    for i in range(wuerfe):
+        A.append(sum([rd.randint(1, seiten+1) for i in range(wuerfel)]))
+        
+    A.sort()
+    for i in A:
+        A.count(i)
+        
+    return A
+        
+        
+f = countsums(5,500,7)
+print(f)        
+        
+        
+        
+#%%
+rd.randint(1, 7+1)        
+        
+        
+# %%
+class MyObject:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
