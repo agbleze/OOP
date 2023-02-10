27a087a9fbfb5bd9d5734254abcf86281d5f4020
Agbleze <lagbleze@st.ug.edu.gh>
2023-02-10 21:08:45 +0100

    Update iterator_pattern.py

--- chap10/iterator_pattern.py
+++ chap10/iterator_pattern.py
@@ -1,3 +1,4 @@
+#%%
 from typing import Iterable, Iterator
 
 
@@ -21,6 +22,22 @@ class CapitalIterator(Iterator[str]):
         word = self.words[self.index]
         self.index += 1
         return word
+
+
+
+
+iterable = CapitalIterable("the quick brown fox jumps over the lazy dog")
+
+
+iterator = iter(iterable)
+
+while True:
+    try:
+        print(next(iterator))
+    except StopIteration:
+        break
+
+# %%
 
 
 
