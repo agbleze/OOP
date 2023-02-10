6645c3926549e7ecae681bae10785dc267d70a8e
Agbleze <lagbleze@st.ug.edu.gh>
2023-02-10 20:46:22 +0100

    Update iterator_pattern.py

--- chap10/iterator_pattern.py
+++ chap10/iterator_pattern.py
@@ -5,7 +5,22 @@ class CapitalIterable(Iterable[str]):
     def __init__(self, string: str) -> None:
         self.string = string
         
+    def __iter__(self) -> Iterable[str]:
+        return CapitalIterator(self.string)
         
+        
+class CapitalIterator(Iterator[str]):
+    def __init__(self, string: str) -> None:
+        self.words = [w.capitalize() for w in string.split()]
+        self.index = 0
+        
+    def __next__(self) -> str:
+        if self.index == len(self.words):
+            raise StopIteration()
+        
+        word = self.words[self.index]
+        self.index += 1
+        return word
 
 
 
