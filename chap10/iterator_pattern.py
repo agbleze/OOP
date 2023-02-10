7f883f392497b091c4f791d9946e9b062cb121d3
Agbleze <lagbleze@st.ug.edu.gh>
2023-02-10 20:31:49 +0100

    Create iterator_pattern.py

---
+++ chap10/iterator_pattern.py
@@ -0,0 +1,13 @@
+from typing import Iterable, Iterator
+
+
+class CapitalIterable(Iterable[str]):
+    def __init__(self, string: str) -> None:
+        self.string = string
+        
+        
+
+
+
+
+
