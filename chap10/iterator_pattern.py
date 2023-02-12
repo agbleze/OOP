22ceb1cf3af2c4cec7abcf2b3f21f89e7e3a3244
Agbleze <lagbleze@st.ug.edu.gh>
2023-02-12 16:44:19 +0100

    Update iterator_pattern.py

--- chap10/iterator_pattern.py
+++ chap10/iterator_pattern.py
@@ -40,7 +40,38 @@ while True:
 # %% list comprehension
 input_strings = ["1", "5", "28", "131", "3"]
 
-output_integers = [int(num) for num in input_strings]
+output_integers = [int(num) for num in input_strings if len(num) < 3]
+
+
+
+# %% set and dict comprehension
+from typing import NamedTuple
+
+class Book(NamedTuple):
+    author: str
+    title: str
+    genre: str
+    
+    
+book = [
+    Book("Pratchett", "Nightwatch", "fantasy"),
+    Book("Pratchett", "Thief Of Time", "fantasy"),
+    Book("Le Guin", "The Dispossessed", "scifi"),
+    Book("Le Guin", "A Wizard Of Earthsea", "fantasy"),
+    Book("Jemisin", "The Broken Earth", "fantasy"),
+    Book("Turner", "The Thief", "fantasy"),
+    Book("Phillips", "Preston Diamond", "western"),
+    Book("Phillips", "Twice Upon A Time", "scifi"),
+]
+
+fantasy_authors = {b.author for b in book if b.genre == "fantasy"}
+fantasy_authors
+
+
+# %%
+fantasy_title = {b.title: b for b in book if b.genre == "fantasy"}
+fantasy_title
+
 
 
 
