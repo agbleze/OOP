26ce58c061c207d8b6db617c6a9b857aa63f226c
Agbleze <lagbleze@st.ug.edu.gh>
2023-10-07 20:31:14 +0200

    Update command_pattern.py

--- chap11/command_pattern.py
+++ chap11/command_pattern.py
@@ -1,9 +1,21 @@
 
 
-#### command strategy #####
+#### strategy pattern #####
 import abc
 from pathlib import Path
+from PIL import Image
+from typing import Tuple
 
+Size = Tuple[int, int]
+
+class FillAlgorithm(abc.ABC):
+    @abc.abstractmethod
+    def make_background(self, img_file: Path,
+                        desktop_size: Size
+                        ) -> Image:
+        pass
+    
+    
 
 
 
