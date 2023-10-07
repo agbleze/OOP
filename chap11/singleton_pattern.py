100ffd2bd7ef58962a730adc43a7080eec291b1c
Agbleze <lagbleze@st.ug.edu.gh>
2023-10-07 23:49:45 +0200

    Update singleton_pattern.py

--- chap11/singleton_pattern.py
+++ chap11/singleton_pattern.py
@@ -1,10 +1,16 @@
 ###### singleton pattern --- not recommended for use in python
 
-
+#%%
 class OnlyOne:
     _single = None
     def __new__(cls, *args, **kwargs):  ### create a new instance when called first time
         if not cls._single:
-            cls._single = cls.__super__.__new__(cls, *args, **kwargs)
+            cls._single = super().__new__(cls, *args, **kwargs)
         return cls._single
         
+        
+o1 = OnlyOne()
+o2 = OnlyOne()
+
+o1 == o2
+# %%
